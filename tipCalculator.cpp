9a92dfd6dc1efbc7b6b52359c0a703d4cb033a7b
perrykay <perryagyemang09@gmail.com>
2020-11-05 21:24:20 +0000

    my first commit

---
+++ tipCalculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//    int numberoffriends;
+//    double amountofbill;
+//    int percentagetip;
+//    cout << "Please enter the number of people" << endl;
+//    cin >> numberoffriends;
+//    cout << "Please enter the bill" << endl;
+//    cin >> amountofbill;
+//    cout << "What is the percentage tip?"<< endl;
+//    cin >> percentagetip;
+//    double actualpercentage = percentagetip / 100;
+//    double total = (actualpercentage * amountofbill) / numberoffriends;
+//    cout << "Each person will pay" << total << "cedis" << endl;
+    cout  << "hello world" << endl;
+
+    return 0;
+}
